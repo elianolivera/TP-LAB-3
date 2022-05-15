b73c1a399c14fd1cf4aa825c02ba9500118a404f
elianolivera <72286803+elianolivera@users.noreply.github.com>
2022-05-15 19:43:58 -0300

    Agregando cositsas

--- src/Practica/Main.java
+++ src/Practica/Main.java
@@ -30,6 +30,11 @@ public class Main {
                 System.out.print("\n ==== editadopa  ===: \n");
                 System.out.print("\n ==== editadopa  ===: \n");
                 System.out.print("\n ====  QUE ONDA WACHO  ===: \n");
+                System.out.print("\n ====  QUE ONDA WACHO  ===: \n");
+                System.out.print("\n ====  QUE ONDA WACHO  ===: \n");
+                System.out.print("\n ====  QUE ONDA WACHO  ===: \n");
+                System.out.print("\n ====  QUE ONDA WACHO  ===: \n");
+                System.out.print("\n ====  QUE ONDA WACHO  ===: \n");
                 switch (Opcion) {
                     case 1:
                         System.out.println(" Mis activos \n");
