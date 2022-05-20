0b129bcd938970b3972939b8e2c6a2b6af2ae26d
leandrob0 <leandro.bovino@gmail.com>
2022-05-19 21:03:53 -0300

    comentario para la clase sesion agregado

--- src/Clases/Sesion.java
+++ src/Clases/Sesion.java
@@ -1,5 +1,6 @@
 package Clases;
 
+// En el caso de que quiera hacer algo como agregar datos a un archivo al apretar salir me parecio mejor usar una clase sesion.
 public final class Sesion {
     private boolean activo;
 
