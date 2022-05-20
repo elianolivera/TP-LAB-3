4e95f7f37604b71b5a349b1e35ac53dd39c1e3e5
elianolivera <72286803+elianolivera@users.noreply.github.com>
2022-05-19 21:27:05 -0300

    Probando commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.print("\n ========  HOLA : ======== ");
+        System.out.print("\n ========  HOLAA : ======== ");
            /* ///EJEMPLO SIMPLE DE TRANSACCION
             User us1  = new User(1,"Pedro","pedr",800);
             User us2  = new User(2,"Robert","pedr",600);
