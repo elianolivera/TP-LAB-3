05ac38d3d603338bdb169d35dfea5f457bde30d6
elianolivera <72286803+elianolivera@users.noreply.github.com>
2022-05-23 02:09:24 -0300

    Funcion transferencia cambios

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,21 @@ import Clases.Sesion;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
 
 
-           /* ///EJEMPLO SIMPLE DE TRANSACCION
+
+/*
               System.out.print("\n ========  PROBANDO MÉTODO TRANSACCIÓN : ======== ");
             User us1  = new User(1,"Pedro","pedr",800);
             User us2  = new User(2,"Robert","pedr",600);
             Tranferencia t1= new Tranferencia();
             t1.transferir(t1,us1,us2,100);
             System.out.println(us1);
-            System.out.println(us2);*/
-
+            System.out.println(us2);
+*/
         Sesion sesion = new Sesion();
         Menu menu = new Menu();
 
